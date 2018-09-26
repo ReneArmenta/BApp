3eda06b9a203a976611051c4f07fb111aad3dc10
Ramiro Armenta <ramiroae@live.com>
2018-09-26 12:01:45 -0600

    Diseño de frontend de la vista "Login"
    -Se ha diseñado el frontend de la vista login.

---
+++ Views/login.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>BeerApp - login</title>
+       
+        <link rel="stylesheet" href="../Assets/css/estilos.css">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link rel="
+        stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container d-flex h-100">
+            <div class="row justify-content-center align-self-center w-100">
+                <div class="col-lg-4" id="login-form">
+                   <h1>BeerApp</h1>
+                    <input type="text" placeholder="Direccion Email">
+                    <input type="password" placeholder="Contraseña">
+                    <button >INICIAR SESIÓN</button>
+                </div>
+                <div class="col-lg-4" id="new-account-text">
+                    <div class="border"></div>
+                    <p>¿No tienes cuenta? <a href="#">Crea una aquí</a></p>
+                </div>
+                
+            </div>
+        </div>
+    </body>
+</html>
