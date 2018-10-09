a566c26592bb5905afeac7062a60cef08faa4cbc
ElDeLaHurley <43621614+ElDeLaHurley@users.noreply.github.com>
2018-10-08 22:05:45 -0500

    animaciones
    -animaciones de items

--- Views/catalogo.php
+++ Views/catalogo.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../Assets/css/lib/animate.css">
     </head>
     <body>
         <div id="view-catalogo" class="container">
@@ -30,12 +31,14 @@
                 </div>
             </div>
         </div>
+        
+        
         <div id="item_cat" class="container">
             <div class="row">
                 <div class="col-6">
-                    <div class="item">
+                    <div id="btn_cerveza" class="item">
                         <img src="../Assets/img/Catalogo/cerveza.png">
-                        <div><p>Cervesa</p></div>
+                        <div><p>Cerveza</p></div>
                     </div>
                 </div>
                 <div class="col-6">
@@ -58,6 +61,40 @@
                 </div>
             </div>
         </div>
+        
+        <!--Marcas-->
+        <div id="cat_marcas">
+            <div class="row">
+                <div class="Col-4">
+                    <div id="item">
+                        <img src="../Assets/img/Cervesas/tecate.png">
+                        <p style="color:white">Tecate</p>
+                        </div>
+                    </div>
+                <div class="Col-4">
+                    <div id="item">
+                        <img src="../Assets/img/Cervesas/Bud.png">
+                        <p style="color:white">bud light</p>
+                    </div>
+                </div>
+                <div class="Col-4">
+                    <div id="item">
+                        <img src="../Assets/img/Cervesas/vino.png">
+                        <p style="color:white">Heineken</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="Col-4">
+                    <div id="item">
+                        <img src="../Assets/img/Cervesas/modelo%20(2).png">
+                        <p style="color:white">Modelo</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
         <footer>
            <div id="todo_ese_pedo">
                 <div class="row">
@@ -70,5 +107,24 @@
                 </div>
             </div>
         </footer>
+        <script src="../Assets/js/login/main-login.js"></script>
+        <script>
+            $("#btn_cerveza").on("click", function(){
+                    $('#item_cat').animateCss('slideOutleft', function() {
+                        $('#item_cat').css("display","none");
+                        $('#cat_marcas').css("display","block");
+                        $('#cat_marcas').animateCss('slideInRight');
+                    
+                });
+            });
+            $("#btn_return").on("click", function(){
+                    $('#cat_marcas').animateCss('slideOutRight', function() {
+                    $('#cat_marcas').css("display","none");
+                        $('#item_cat').css("display","block");
+                        $('#item_cat').animateCss('slideInLeft');
+                });
+            });
+            
+        </script>
     </body>
 </html>
